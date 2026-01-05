655192a51768604244a6b050f4a5d271edd994d4
mister_papaye <philippeetchtebaise@hotmail.com>
2026-01-05 08:30:48 +0100

    add middleware err

--- assets/server.js
+++ assets/server.js
@@ -6,11 +6,27 @@ const path = require("path");
 const morgan = require("morgan");
 const webhook_router = require("./routes/webhook");
 const { isUpdating  } = require("./var/onUpdate");
+const { isMaintenance  } = require("./var/maintenance");
 require("dotenv").config();
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use((err, req, res, next) => {
+    console.error("🔥 Erreur:", err);
+
+    if (req.path.startsWith("/api")) {
+        return res.status(500).json({
+            error: true,
+            morgan: "Erreur interne du serveur",
+        });
+    }
+
+    res.status(500).sendFile(
+        path.join(__dirname, "views", "500.html")
+    );
+});
+
 
 app.use(`/api/webhook`, webhook_router);
 app.use('/api/views', express.static(path.join(__dirname, 'views')));
@@ -18,6 +34,16 @@ app.use('/api/views', express.static(path.join(__dirname, 'views')));
 app.get('/api/maj/status', (req, res) => {
     res.json({ isUpdating: isUpdating() });
 })
+
+app.use((req, res, next) => {
+    if (isMaintenance()) {
+        if (req.path.startsWith('/api/views')) {
+            return next();
+        }
+        return res.sendFile(path.join(__dirname, "views", "maintenance.html"));
+    }
+    next();
+});
 
 app.use((req, res, next) => {
     if (isUpdating()) {
