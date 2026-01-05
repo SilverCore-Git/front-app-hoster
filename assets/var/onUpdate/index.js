119ff95c395f4fb015be98d0ab57a9c198b5657a
mister_papaye <philippeetchtebaise@hotmail.com>
2026-01-05 08:25:35 +0100

    fix

--- assets/var/onUpdate/index.js
+++ assets/var/onUpdate/index.js
@@ -1,29 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, "maintenance.json");
+const filePath = path.join(__dirname, "state.json");
 
 if (!fs.existsSync(filePath)) {
     fs.writeFileSync(filePath, JSON.stringify({ isUpdating: false }));
 }
 
 module.exports = {
-    setMaintenance(val) {
+    setUpdating(val) {
         try {
             const data = { isUpdating: !!val };
             fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
         } catch (err) {
-            console.error("Erreur lors de l'écriture du statut de maintenance :", err);
+            console.error("Erreur lors de l'écriture du statut de mise à jour :", err);
         }
     },
 
-    isMaintenance() {
+    isUpdating() {
         try {
             const data = fs.readFileSync(filePath, "utf8");
             const parsed = JSON.parse(data);
             return parsed.isUpdating;
         } catch (err) {
-            console.error("Erreur lors de la lecture du statut de maintenance :", err);
+            console.error("Erreur lors de la lecture du statut de mise à jour :", err);
             return false;
         }
     }
